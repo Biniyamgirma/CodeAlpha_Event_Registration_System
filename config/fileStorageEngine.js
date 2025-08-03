ba02dd737caa95bd521ac3f47a805e3d424730c9
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-03 20:44:02 +0300

    some modification

---
+++ config/fileStorageEngine.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        const dir = path.join(__dirname, '../uploads');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
+    },
+    filename: (req,file,cb)=>{
+        const uniqueSuffix = Date.now() + '--' + Math.round(Math.random() * 1E9);
+        cd(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+
+exports.constants = {
+    storage: storage
+}
--- router/systemAdminRoute/sponsor.js
+++ router/systemAdminRoute/sponsor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
+const storage = require('../../config/fileStorageEngine').constants.storage;
 const {
     getAllSponsors,
     createSponsor,
@@ -10,9 +11,11 @@ const {
      blockSponsor
 } = require('../../controller/sponsorController/sponsorController');
 
+const upload = multer({ storage: storage });
+
 router.route('/').get(getAllSponsors);
-router.route('/regiter').post(createSponsor);
-router.route('/update').put(updateSponsor);
+router.route('/regiter', upload.single('logo')).post(createSponsor);
+router.route('/update', upload.single('logo')).put(updateSponsor);
 router.route('/sponsor/:id').get(getSponsorById);
 router.route('/block/sponsor/:id').post(blockSponsor);
 router.route('/event/:eventId').get(getSponsorByEventId);
